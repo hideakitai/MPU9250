a3313ee4678ba7b4b1cbd4ebc003e98ad139d3d9
Hideaki Tai <hideaki.tai@gmail.com>
2020-12-21 13:39:12 +0900

    add simple example with calibration

---
+++ examples/simple_with_calibration/simple_with_calibration.ino
@@ -0,0 +1,30 @@
+#include "MPU9250.h"
+
+MPU9250 mpu;
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    delay(2000);
+
+    if (!mpu.setup(0x68)) { // change to your own address
+        while(1) {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+    }
+    mpu.verbose(true);
+
+    // calibrate anytime you want to
+    mpu.calibrateAccelGyro();
+    mpu.calibrateMag();
+    mpu.printCalibration();
+
+    mpu.verbose(false);
+}
+
+void loop() {
+    if (mpu.update()) {
+        mpu.printRollPitchYaw();
+    }
+}
