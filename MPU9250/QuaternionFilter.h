7d34ad2479466494fa9fe6ee6f2a1ed19ae4f64b
John Val <j.val@rijnlandslyceum-rlo.nl>
2020-12-30 16:11:49 +0100

    Update QuaternionFilter.h
    In this version next to the Madgwick filter (QUATERNION_FILTER == 1), which corrects gyro integration using acceleration and magnetometer, an update routine without filtering (QUATERNION_FILTER == 0) and a version of the Mahony filter (QUATERNION_FILTER == 2) only using acceleration for filtering are present.

--- MPU9250/QuaternionFilter.h
+++ MPU9250/QuaternionFilter.h
@@ -2,7 +2,14 @@
 #ifndef QUATERNIONFILTER_H
 #define QUATERNIONFILTER_H
 
+#ifndef QUATERNION_FILTER
+	#define QUATERNION_FILTER 1
+#endif
+
 class QuaternionFilter {
+#if QUATERNION_FILTER == 1
+// Madgwick accel magnetometer filter
+
     float GyroMeasError = PI * (40.0f / 180.0f);     // gyroscope measurement error in rads/s (start at 40 deg/s)
     float GyroMeasDrift = PI * (0.0f / 180.0f);      // gyroscope measurement drift in rad/s/s (start at 0.0 deg/s/s)
     float beta = sqrt(3.0f / 4.0f) * GyroMeasError;  // compute beta
@@ -107,6 +114,109 @@ public:
         q[2] = q2;
         q[3] = q3;
     }
+#endif
+
+#if QUATERNION_FILTER == 0
+	// No filter simply integrate gyro
+public:
+    void bind() {}
+ 	void update(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz, float *q, float deltaT ) {
+        float q0 = q[0], q1 = q[1], q2 = q[2], q3 = q[3];  // variable for readability
+        q[0] += 0.5f * (-q1 * gx - q2 * gy - q3 * gz)*deltaT;
+        q[1] += 0.5f * (q0 * gx + q2 * gz - q3 * gy)*deltaT;
+        q[2] += 0.5f * (q0 * gy - q1 * gz + q3 * gx)*deltaT;
+        q[3] += 0.5f * (q0 * gz + q1 * gy - q2 * gx)*deltaT;
+		float recipNorm = 1.0 / sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
+        q[0] *= recipNorm;
+        q[1] *= recipNorm;
+        q[2] *= recipNorm;
+        q[3] *= recipNorm;
+	}
+#endif
+
+#if QUATERNION_FILTER == 2
+	// Mahony accelleration filter
+	// Mahony scheme uses proportional and integral filtering on
+	// the error between estimated reference vector (gravity) and measured one.
+	// Madgwick's implementation of Mayhony's AHRS algorithm.
+	// See: http://www.x-io.co.uk/node/8#open_source_ahrs_and_imu_algorithms
+	// Free parameters in the Mahony filter and fusion scheme,
+	// Kp for proportional feedback, Ki for integral
+	float Kp = 30.0;
+	float Ki = 0.0;
+	// with MPU-9250, angles start oscillating at Kp=40. Ki does not seem to help and is not required.
+	// with MPU-6050, some instability observed at Kp=100 Now set to 30.
+public:
+	void bind() {}
+    void update(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz, float *q, float deltaT ) {
+		float recipNorm;
+		float vx, vy, vz;
+		float ex, ey, ez;  //error terms
+		float qa, qb, qc;
+		static float ix = 0.0, iy = 0.0, iz = 0.0;  //integral feedback terms
+		float tmp;
+
+		// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+		tmp = ax * ax + ay * ay + az * az;
+		if (tmp > 0.0)
+		{
+
+			// Normalise accelerometer (assumed to measure the direction of gravity in body frame)
+			recipNorm = 1.0 / sqrt(tmp);
+			ax *= recipNorm;
+			ay *= recipNorm;
+			az *= recipNorm;
+
+			// Estimated direction of gravity in the body frame (factor of two divided out)
+			vx = q[1] * q[3] - q[0] * q[2];
+			vy = q[0] * q[1] + q[2] * q[3];
+			vz = q[0] * q[0] - 0.5f + q[3] * q[3];
+
+			// Error is cross product between estimated and measured direction of gravity in body frame
+			// (half the actual magnitude)
+			ex = (ay * vz - az * vy);
+			ey = (az * vx - ax * vz);
+			ez = (ax * vy - ay * vx);
+
+			// Compute and apply to gyro term the integral feedback, if enabled
+			if (Ki > 0.0f) {
+			  ix += Ki * ex * deltaT;  // integral error scaled by Ki
+			  iy += Ki * ey * deltaT;
+			  iz += Ki * ez * deltaT;
+			  gx += ix;  // apply integral feedback
+			  gy += iy;
+			  gz += iz;
+			}
+
+			// Apply proportional feedback to gyro term
+			gx += Kp * ex;
+			gy += Kp * ey;
+			gz += Kp * ez;
+		}
+
+		// Integrate rate of change of quaternion, q cross gyro term
+		deltaT = 0.5 * deltaT;
+		gx *= deltaT;   // pre-multiply common factors
+		gy *= deltaT;
+		gz *= deltaT;
+		qa = q[0];
+		qb = q[1];
+		qc = q[2];
+		q[0] += (-qb * gx - qc * gy - q[3] * gz);
+		q[1] += (qa * gx + qc * gz - q[3] * gy);
+		q[2] += (qa * gy - qb * gz + q[3] * gx);
+		q[3] += (qa * gz + qb * gy - qc * gx);
+
+		// renormalise quaternion
+		recipNorm = 1.0 / sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
+		q[0] = q[0] * recipNorm;
+		q[1] = q[1] * recipNorm;
+		q[2] = q[2] * recipNorm;
+		q[3] = q[3] * recipNorm;
+			
+	}
+	
+#endif
 };
 
 #endif  // QUATERNIONFILTER_H
